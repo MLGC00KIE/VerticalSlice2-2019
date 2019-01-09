d78f104f15b9a8e736be260f9b6dfb91a2fdcce4
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2019-01-09 12:36:34 +0100

    start eye attack

---
+++ PlatformerGame/Assets/Scripts/EnemyEyeAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEyeAttack : MonoBehaviour {
+
+    public Transform Projectile;
+    public int speed = 5;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShootProjectile()
+    {
+        Projectile.transform.position += transform.left * speed * Time.deltaTime;
+    }
+}
