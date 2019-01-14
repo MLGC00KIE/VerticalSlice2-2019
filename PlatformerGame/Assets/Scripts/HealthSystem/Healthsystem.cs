74b01636ca192e11b2d66b97ec626b95b0fbaad6
Toon <toonschaap3@gmail.com>
2019-01-14 10:52:13 +0100

    commit 1
    Begin healthsystem

---
+++ PlatformerGame/Assets/Scripts/HealthSystem/Healthsystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthsystem : MonoBehaviour {
+    private int Health;
+
+	// Use this for initialization
+	void Start () {
+        Health = 3;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision PlayerHitBox)
+    {
+       if(PlayerHitBox.gameObject.tag == "EnemyAttack")
+        {
+
+        }        
+    }
+}
