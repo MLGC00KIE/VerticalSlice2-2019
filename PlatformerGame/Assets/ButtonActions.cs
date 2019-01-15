5a7bd3b69c7c9ed2d368b561f53ec2cbe836a9c3
MLGC00KIE <24893@ma-web.nl>
2019-01-15 11:53:39 +0100

    added working menu just need art

---
+++ PlatformerGame/Assets/ButtonActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
